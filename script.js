1e9f86bc67e49d3838752ad983130c6d83aac138
manjumallesh678 <56191254+manjumallesh678@users.noreply.github.com>
2020-05-24 22:54:13 +0530

    simplejs

---
+++ script.js
@@ -0,0 +1,44 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+*/
+
+// STEP 1:
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+for(var i = 0; i < names.length; i++){
+  if(names[i].charAt(0) == 'j' || names[i].charAt(0) == 'J'){
+    console.log("Good Bye" + names[i]);
+  }
+  else{
+    console.log("Hello " + names[i]);
+  }
+}
